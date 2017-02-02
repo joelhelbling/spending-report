d7fccf0f260e69ef5506ac6682b8dfa2e5a34c17
Joel Helbling <joel@joelhelbling.com>
2017-02-02 12:03:12 -0500

    adds react-bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './css/index.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap-theme.css'
 
 ReactDOM.render(
   <App />,
